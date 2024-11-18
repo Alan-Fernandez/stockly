6c0e2fd7778b322cf6b64800182732126a71f84b
alan damian Fernandez <92124271+Alan-Fernandez@users.noreply.github.com>
2024-11-18 15:45:03 -0300

    nome page

--- app/_components/Sidebar.tsx
+++ app/_components/Sidebar.tsx
@@ -1,3 +1,8 @@
+
+
+import { LayoutGridIcon, PackageIcon, ShoppingBasketIcon } from "lucide-react";
+import SidebarButton from "./Sidebar-button";
+
 const Sidebar = () => {
   return (
     <div className="w-64 bg-white">
@@ -7,9 +12,24 @@ const Sidebar = () => {
       </div>
       {/* BUTTOM */}
       <div className="flex flex-col gap-2 p-2">
-        <button className="px-6 py-3">Deshboard</button>
-        <button className="px-6 py-3">Profuctos</button>
-        <button className="px-6 py-3">Vendas</button>
+        <SidebarButton
+          href="/"
+        >
+          <LayoutGridIcon size={24} />
+          Dashboard
+        </SidebarButton>
+        <SidebarButton
+          href="/products"
+        >
+          <PackageIcon size={24} />
+          Produtos
+        </SidebarButton>
+        <SidebarButton
+          href="/sales"
+        >
+          <ShoppingBasketIcon size={24} />
+          Vendas
+        </SidebarButton>
       </div>
     </div>
   );
